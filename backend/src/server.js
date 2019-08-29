59574449e8727708dad84c8f4789bde012036d71
tabatagloria <tabatabruna@yahoo.com.br>
2019-08-29 16:20:57 -0300

    MongoDB conectado

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 const server = express();
 
+mongoose.connect('mongodb+srv://omnistack:12345@cluster0-7yrfl.mongodb.net/omnistack?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
+
+server.use(express.json());
 server.use(routes);
 
 server.listen(3333);
