1e8bad8bae4d3c1c7bf49a6b2c325adcbd82961b
tabatagloria <tabatabruna@yahoo.com.br>
2019-08-29 18:49:14 -0300

    Match funcionando

--- backend/src/controllers/LikeController.js
+++ backend/src/controllers/LikeController.js
@@ -1,10 +1,26 @@
 const Dev = require('../models/Dev');
 
 module.exports = {
-    store(req, res){
-        console.log(req.params.devId);
-        console.log(req.headers.user);
+    async store(req, res){
+        const {user} = req.headers;
+        const {devId} = req.params;
+
+        const loggedDev = await Dev.findById(user);
+        const targetDev = await Dev.findById(devId);
+
+        if(!targetDev) {
+            return res.status(400).json({error: 'Dev not exists'});
+        }
+
+        if(targetDev.likes.includes(loggedDev._id)){
+            console.log('DEU MATCH');            
+        }
+
+        loggedDev.likes.push(targetDev._id);
+
+        await loggedDev.save();
+
         
-        return res.json({ok: true});
+        return res.json(loggedDev);
     }
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 const server = express();
 
-mongoose.connect('mongodb+srv://omnistack:12345@cluster0-7yrfl.mongodb.net/omnistack?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://socorro:12345@cluster0-o0vut.mongodb.net/teste2?retryWrites=true&w=majority', {
     useNewUrlParser: true
 });
 
 server.use(express.json());
 server.use(routes);
 
-server.listen(3333);
+server.listen(8100);
