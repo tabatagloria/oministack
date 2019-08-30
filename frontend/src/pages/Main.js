cf36665e4bdd888f4f8563125a1c0be43d42bf03
tabatagloria <tabatabruna@yahoo.com.br>
2019-08-30 10:23:09 -0300

    Instalado Socket

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import io from 'socket.io-client';
 import {Link} from 'react-router-dom';
 import './Main.css';
 import api from '../services/api';
@@ -20,6 +21,10 @@ export default function Main({match}){
             setUsers(response.data);
         }
         loadUsers();
+    }, [match.params.id]);
+
+    useEffect(() => {
+        const socket = io('http://localhost:8181');
     }, [match.params.id]);
 
     async function handleLike(id) {
